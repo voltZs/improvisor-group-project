2baae4517d286b964cfdee809aeca58fa3d9d080
Alex <alex.mcgill@outlook.com>
2019-02-12 12:26:54 +0000

    Integrated Zsolt's current / frequent implementation

--- improvisor/routes.py
+++ improvisor/routes.py
@@ -1,14 +1,119 @@
-from flask import Flask, render_template
-from improvisor import app, socketio
+from flask import Flask, render_template, request, redirect, abort, session
+from improvisor import app, socketio, sample_files
+from operator import itemgetter
+import json
+
+# Retrieve the sample assets from sample_files.py
+assets = sample_files.assets
+
 
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
-@app.route('/controller', methods=['GET'])
-def controller():
-	return render_template('controller.html')
 
 @app.route('/presenter', methods=['GET'])
 def presenter():
 	return render_template('presenter.html')
+
+
+@app.route('/controller', methods=['GET'])
+def controller():
+	return render_template('controller.html')
+
+
+@app.route('/compare_phrases', methods=['POST'])
+def compare_phrases():
+
+    phrase = request.form.get('phrase')
+    mentioned_tags = request.form.get('mentionedTags')
+    flush_recent = request.args.get("flush")
+
+    if not mentioned_tags:
+        mentioned_tags = { 'recent' : {}, 'all' : {} }
+    else:
+        mentioned_tags = json.loads(mentioned_tags)
+
+    # CREATE LIST OF ALL EXISTING TAGS ON ASSETS - THIS WOULD BE STORED OFC
+    tag_pool = []
+    for asset in assets:
+        for tag in asset['tags']:
+            if tag not in tag_pool:
+                tag_pool.append(tag)
+
+    sorting_obj = {
+		'assetResults' : {
+			'current' : [],
+			'frequent' :[]},
+        'mentionedTags' : {}
+            }
+
+    # EMPTY RECENT TAGS IF THE REQUEST HAS FLUSH SET TO TRUE
+    if flush_recent == "true":
+        mentioned_tags['recent'] = {}
+
+    # UPDATE THE MENTIONED_TAGS OBJECT
+    for word in phrase.split(' '):
+        word = word.lower()
+        if word in tag_pool:
+            # IF THE WORD IS NOT IN THE TAGS, INITIALISE - SSEPARATELY FOR RECENT AND ALL
+            all_tags = mentioned_tags.get('all')
+            recent_tags = mentioned_tags.get('recent')
+            if not all_tags.get(word):
+                mentioned_tags['all'][word] = {'mentions' : 0}
+                print("Initialised mentioned_tags['all']['" + word + "'] in ALL")
+            if not recent_tags.get(word):
+                mentioned_tags['recent'][word] = {'mentions' : 0}
+                print("Initialised mentioned_tags['all']['" + word + "'] in RECENT")
+
+            # ADD THE NEW TAG TO THE MENTIONED_TAGS OBJ
+            mentioned_tags['all'][word]['mentions'] += 1
+            mentioned_tags['recent'][word]['mentions'] += 1
+            print("Adding mention to " + word + " in  RECENT")
+        # recent_tags = sortingObj.get(mentionedTags).get('recent')
+        # session['recent']
+        # sesssion['frequent']
+
+    sorting_obj['mentionedTags'] = mentioned_tags
+
+    frequent_assets = []
+    current_assets = []
+
+    # MANAGE ADDING asset_selection
+    for asset in assets:
+        ###### FOR ALL -> FREQUENT
+        mentioned_all = mentioned_tags['all'].keys()
+        asset['weight'] = 0
+        # LOOP FOR UPDATING WEIGHT
+        for tag in mentioned_all:
+            if tag in asset['tags']:
+                asset['weight'] += mentioned_tags['all'][tag]['mentions']
+
+        # LOOP TO ADD FOUND ONES, BREAK SO IT ONLY ADDS ASSET ONCE IF
+        # IT HAS MORE THAN ONE MATCHING TAG
+        for tag in mentioned_all:
+            if tag in asset['tags']:
+                frequent_assets.append(asset)
+                break
+
+        ###### FOR RECENT -> CURRENT
+        mentioned_recent = mentioned_tags['recent'].keys()
+        asset['weight'] = 0
+        # LOOP FOR UPDATING WEIGHT
+        for tag in mentioned_recent:
+            if tag in asset['tags']:
+                asset['weight'] += mentioned_tags['recent'][tag]['mentions']
+
+        # LOOP TO ADD FOUND ONES, BREAK SO IT ONLY ADDS ASSET ONCE IF
+        # IT HAS MORE THAN ONE MATCHING TAG
+        for tag in mentioned_recent:
+            if tag in asset['tags']:
+                current_assets.append(asset)
+                break
+
+    sorted_frequent =  sorted(frequent_assets, key=itemgetter('weight'), reverse=True)
+    sorted_current = sorted(current_assets, key=itemgetter('weight'), reverse=True)
+    sorting_obj['assetResults']['frequent'] = sorted_frequent
+    sorting_obj['assetResults']['current'] = sorted_current
+
+    return json.dumps(sorting_obj)
---
+++ improvisor/sample_files.py
@@ -0,0 +1,103 @@
+assets = [
+    {
+        "id" : 1,
+        "title": " ",
+        "tags": ['perfume', 'slovakia', 'givenchy', 'gentleman'],
+        "thumbnail": "1.jpeg"
+    },{
+        "id" : 2,
+        "title": " ",
+        "tags": ['desert', 'sweet', 'food', 'fork', 'carbs'],
+        "thumbnail": "2.jpeg"
+    },{
+        "id" : 3,
+        "title": " ",
+        "tags": ['food', 'chicken', 'cheese', 'healthy', 'grill'],
+        "thumbnail": "3.jpeg"
+    },{
+        "id" : 4,
+        "title": " ",
+        "tags": ['building', 'edinburgh', 'day', 'north bridge', 'sky'],
+        "thumbnail": "4.jpeg"
+    },{
+        "id" : 5,
+        "title": " ",
+        "tags": ['error', 'dark', 'screen', 'text'],
+        "thumbnail": "5.jpeg"
+    },{
+        "id" : 6,
+        "title": " ",
+        "tags": ['pancake', 'pancakes', 'desert', 'raspberry', 'fruit', 'sweet', 'nutella', 'banana'],
+        "thumbnail": "6.jpeg"
+    },{
+        "id" : 7,
+        "title": " ",
+        "tags": ['meme', 'cat', 'stretch', 'food', 'cheese'],
+        "thumbnail": "7.jpeg"
+    },{
+        "id" : 8,
+        "title": " ",
+        "tags": ['cold', 'snow', 'window', 'nature'],
+        "thumbnail": "8.jpeg"
+    },{
+        "id" : 9,
+        "title": " ",
+        "tags": ['window', 'office', 'night', 'edinburgh'],
+        "thumbnail": "9.jpeg"
+    },{
+        "id" : 10,
+        "title": " ",
+        "tags": ['edinburgh', 'park', 'monument', 'twilight'],
+        "thumbnail": "10.jpeg"
+    },{
+        "id" : 11,
+        "title": " ",
+        "tags": ['phone', 'case', 'phonecase', 'hand', 'iphone', 'aztec', 'ornament'],
+        "thumbnail": "11.jpeg"
+    },{
+        "id" : 12,
+        "title": " ",
+        "tags": ['food', 'burger', 'note', 'beef', 'cheese'],
+        "thumbnail": "12.jpeg"
+    },{
+        "id" : 13,
+        "title": " ",
+        "tags": ['edinburgh', 'day', 'sky', 'building', 'architecture', 'tollcross'],
+        "thumbnail": "13.jpeg"
+    },{
+        "id" : 14,
+        "title": " ",
+        "tags": ['edinburgh', 'day', 'sky', 'building', 'architecture', 'tollcross'],
+        "thumbnail": "14.jpeg"
+    },{
+        "id" : 15,
+        "title": " ",
+        "tags": ['screenshot', 'phone', 'instagram', 'funny'],
+        "thumbnail": "15.png"
+    },{
+        "id" : 16,
+        "title": " ",
+        "tags": ['books', 'book', 'antique', 'leather', 'shelf'],
+        "thumbnail": "16.jpg"
+    },{
+        "id" : 17,
+        "title": " ",
+        "tags": ['graph', 'hand', 'screen', 'lie', 'detector'],
+        "thumbnail": "17.jpg"
+    },{
+        "id" : 18,
+        "title": " ",
+        "tags": ['screenshot', 'music', 'mountain', 'high', 'enough', 'phone'],
+        "thumbnail": "18.png"
+    },{
+        "id" : 19,
+        "title": " ",
+        "tags": ['screenshot', 'music', 'elley', 'counterfeit', 'phone'],
+        "thumbnail": "19.png"
+    },{
+        "id" : 20,
+        "title": " ",
+        "tags": ['meme', 'adele', 'hello', 'funny'],
+        "thumbnail": "20.png"
+    }
+]
