356106c4b4031234c40da2c31a29928decb172bc
Ross <ross.s.cockburn@gmail.com>
2019-02-25 21:34:51 +0000

    added creation data to asset table in db

--- improvisor/models/asset_model.py
+++ improvisor/models/asset_model.py
@@ -1,6 +1,7 @@
 from db import db
 from improvisor.models.associationTable_tag_asset import asset_tags
 from flask import session
+import datetime
 
 class AssetModel(db.Model):
     __tablename__ = "assets"
@@ -9,19 +10,22 @@ class AssetModel(db.Model):
     assetname = db.Column(db.String(200))
     assetLocation = db.Column(db.String(200), nullable = True)
     thumbnailLocation = db.Column(db.String(200), nullable=True)
+    dateCreated = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
     
     user = db.relationship("UserModel")
     tags = db.relationship("TagModel",secondary=asset_tags, lazy="subquery", backref=db.backref("assets", lazy=True))
 
     def json(self):
-        return {"asset": self.assetname, "tags" : [tag.json() for tag in self.tags],"user": self.user_id, "assetLocation" : self.assetLocation, "thumbnailLocation" : self.thumbnailLocation}
+        return {"asset": self.assetname, "tags" : [tag.json() for tag in self.tags],"user": self.user_id, "assetLocation" : self.assetLocation, "thumbnailLocation" : self.thumbnailLocation, "date-created" : self.dateCreated}
 
-    def __init__(self, assetname, user_id, assetLocation = None, thumbnailLocation = None):
+    def __init__(self, assetname, user_id, assetLocation = None, thumbnailLocation = None, dateCreated = datetime.datetime.utcnow()):
         self.assetname = assetname 
         self.user_id = user_id
         self.assetLocation = assetLocation
         self.thumbnailLocation = thumbnailLocation
+        self.dateCreated = dateCreated
         
         
     def save_to_db(self):
