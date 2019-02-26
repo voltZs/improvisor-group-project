332e31098c8c7925e4f85038e0eea288208f9d7d
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-02-26 16:26:42 +0000

    Increased maximum file size upload limit to 32MB

--- improvisor/config/defaults.py
+++ improvisor/config/defaults.py
@@ -4,3 +4,4 @@ SQLALCHEMY_DATABASE_URI =  "sqlite:///data.db"
 PROPAGATE_EXCEPTIONS = True
 # Secret key
 SECRET_KEY = "b'\\x01IO\\x08\\x08L\\xe8Nia5\\x82\\x98\\xa5\\xc6\\xd3\\xf1\\xcf`\\xa3\\xb8G4\\xf8"
+MAX_CONTENT_LENGTH = 32 * 1024 * 1024
