946db67ee0bf2d2028e18918d169469d6d02156e
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-03-02 12:50:36 +0000

    Fixed asset tab thumbnail bug when thumbnail null

--- improvisor/static/JS/controller.js
+++ improvisor/static/JS/controller.js
@@ -277,7 +277,12 @@ function populateActiveTab() {
   tabRow.innerHTML = "";
   for (asset in assets) {
     var image = document.createElement("IMG");
-    image.src = assets[asset]['thumbnailLocation'];
+    var thumbnail = assets[asset]['thumbnailLocation'];
+    if (thumbnail != null) {
+      image.src = thumbnail;
+    } else {
+      image.src = "https://i.imgur.com/5NqcCVN.png";
+    }
     image.setAttribute("data-id", assets[asset]['id']);
     image.classList.add("assetThumbnail");
     image.classList.add("animated");
