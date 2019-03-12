cb0396236c92bc912ec4acd24d8a3169e6097607
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-03-12 23:44:13 +0000

    Minor change to date_model

--- improvisor/models/date_model.py
+++ improvisor/models/date_model.py
@@ -24,6 +24,10 @@ class DateModel(db.Model):
         self.dateAdded = dateAdded
         self.tab = tab
 
+    #@classmethod
+    #def find_by_sessionId(cls, id):
+    #    return cls.query.filter_by(session_id=id, user_id=current_user.get_id()).first()
+    # We should be searching by session number not by session id. Also means we need to change session_id ^ up top to session_num
     @classmethod
-    def find_by_sessionId(cls, id):
-        return cls.query.filter_by(session_id=id, user_id=current_user.get_id()).first()
+    def find_by_sessionNum(cls, id):
+        return cls.query.filter_by(session=id, user_id=current_user.get_id()).first()
