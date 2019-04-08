bc91b6f3524b50b7da7d21ffac4489710c01957b
Zsolt Varga <zs.zsolt.varga@gmail.com>
2019-04-08 23:44:42 +0100

    Adding reset button for color picker

--- improvisor/static/JS/user_settings.js
+++ improvisor/static/JS/user_settings.js
@@ -1,5 +1,6 @@
 var profileImgBtn = document.getElementById("profileImgBtn");
 var userPictureInput = document.getElementById("userPicture");
+var resetColorBtn = document.getElementById("resetColor");
 
 $(userPictureInput).parent().hide();
 
@@ -9,4 +10,8 @@ profileImgBtn.addEventListener("click", function(){
 
 userPictureInput.addEventListener("change", function(){
   document.getElementById("userForm").submit();
+})
+
+resetColorBtn.addEventListener("click", function(){
+  document.getElementById("colourSetting").value = "#2664c9";
 })
